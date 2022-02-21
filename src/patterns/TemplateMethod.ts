8040ebf2a5283bfe494d2a83cf40d53776169067
deal-machine <95deal@gmail.com>
2022-02-20 22:09:33 -0300

    studying template method pattern

--- src/patterns/TemplateMethod.ts
+++ src/patterns/TemplateMethod.ts
@@ -1,1 +1,50 @@
-console.log("Template Method Pattern");
+abstract class Character {
+  public templateMethod() {
+    this.walk();
+    this.jump();
+    this.fight();
+    this.magicPower();
+  }
+  protected walk(): void {
+    console.log("Walking...");
+  }
+
+  protected jump(): void {
+    console.log("Jumping...");
+  }
+
+  protected abstract fight(): void;
+
+  protected abstract magicPower(): void;
+}
+
+class Warrior extends Character {
+  protected fight() {
+    console.log("Fighting...");
+  }
+  protected magicPower(): void {
+    console.log("Cannot use magic...");
+  }
+}
+
+class Wizard extends Character {
+  protected walk(): void {
+    console.log("Slowly Walking...");
+  }
+
+  protected jump(): void {
+    console.log("Cannot Jump...");
+  }
+  protected fight() {
+    console.log("Cannot Fight...");
+  }
+  protected magicPower(): void {
+    console.log("Powered by magic...");
+  }
+}
+
+(() => {
+  new Warrior().templateMethod();
+
+  new Wizard().templateMethod();
+})();
